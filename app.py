3388bd12d592a998fa1ee8abfebcdb2ffd12a170
Collin Plank <collin.plank@gmail.com>
2024-12-26 13:37:21 -0500

    Add show action

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/products.json")
 def index():
     return db.products_all()
+
+@app.route("/products/<id>.json")
+def show(id):
+    return db.products_find_by_id(id)
