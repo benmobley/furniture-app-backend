9b3421957dace787132e6df35d30250df454b864
mkanwal@iit <70186630+mkanwal-iit@users.noreply.github.com>
2024-12-26 12:55:02 -0600

    update action

--- app.py
+++ app.py
@@ -16,3 +16,14 @@ def update(id):
     category = request.form.get("category") or product["category"]
     return db.products_update_by_id(id, name, description, price, category)
 
+@app.route("/products.json", methods=["POST"])
+def create():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    price = request.form.get("price")
+    category = request.form.get("category")
+    return db.products_create(name, description, price, category)
+
+@app.route("/products/<id>.json")
+def show(id):
+    return db.products_find_by_id(id)
